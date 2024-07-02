a732108319d7f24cbeef3b4684b1654ff7950c1a
Ashish Singh <ashish.21bce7889@vitapstudent.ac.in>
2024-07-02 09:34:47 +0530

    d2

---
+++ lastOneBinary.java
@@ -0,0 +1,18 @@
+public class lastOneBinary {
+    public static void main(String[] args) {
+        int[] arr = { 0, 0, 1, 1, 1, 1 };
+        int l = 0;
+        int r = arr.length - 1;
+        int res = -1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (arr[mid] == 1) {
+                res = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        System.out.println(res);
+    }
+}
